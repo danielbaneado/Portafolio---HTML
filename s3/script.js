5907355bb18f0be207500e84fe6d596772d9adad
leonela miranda <leonelamiranda141024@gmail.com>
2026-04-23 13:36:31 -0500

    task 4.5

--- s3/script.js
+++ s3/script.js
@@ -1,1 +1,14 @@
-console.log("Holanda")
+const button= document.getElementById("hidden");
+
+button.addEventListener("click", touch)
+
+function touch() {
+  var boton= document.getElementById("hidden");
+  var img= document.getElementById("img");
+  if (boton.innerText=== "Te esquivé") {
+    boton.innerText= "Tocame";
+  } else {
+    alert("Jaja");
+    boton.innerText= "Te esquivé";
+  }
+}
