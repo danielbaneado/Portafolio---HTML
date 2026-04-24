931ed9e80ba10068006f746117d752f32739d8e6
JorgeCb12 <jorgeluiscorralesbarraza272@gmail.com>
2026-04-24 07:14:59 -0500

    task 5

--- s3/script.js
+++ s3/script.js
@@ -4,11 +4,16 @@ button.addEventListener("click", touch)
 
 function touch() {
   var boton= document.getElementById("hidden");
-  var img= document.getElementById("img");
+  var img= document.querySelector(".goku");
+  var cont= document.querySelector(".evento");
   if (boton.innerText=== "Te esquivé") {
     boton.innerText= "Tocame";
+    img.style.display= "none";
+    
   } else {
-    alert("Jaja");
     boton.innerText= "Te esquivé";
+    img.style.display= "block";
+    cont.style.top = Math.random() * 100 + "%";
+    cont.style.left = Math.random() * 100 + "%";
   }
 }
